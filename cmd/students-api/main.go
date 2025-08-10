8e34e33f2eb9f04c2e353d0ccaed6bcfc0727664
MadhavKrishanGoswami <goswamimadhavkrishan@gmail.com>
2025-08-11 04:36:45 +0530

    Graceful Shutdown added

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/MadhavKrishanGoswami/students-api/internal/config"
 )
@@ -18,15 +24,31 @@ func main() {
 	})
 
 	// 3. Setup Server
+	slog.Info("server started", "address", cfg.Addr)
 	log.Printf("Starting server on %s", cfg.Addr)
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	// 4. Start the server
-	err := server.ListenAndServe()
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+	<-done
+
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
+		slog.Error("Failed to shutdown server", "error", err)
 	}
+	slog.Info("Server gracefully stopped")
 }
